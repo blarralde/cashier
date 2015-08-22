3b20657719634cb86af34190013eac8a7ea361ef
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-08-21 17:46:26 -0700

    Use ConnectionPool instead of Redis
    Use ConnectionPool to support multiple connections to Redis at once.

--- lib/cashier/adapters/redis_store.rb
+++ lib/cashier/adapters/redis_store.rb
@@ -6,36 +6,60 @@ module Cashier
       end
 
       def self.redis=(redis_instance)
-        @@redis = redis_instance
+        @@redis = if redis_instance.is_a? ConnectionPool
+          redis_instance
+        else
+          ConnectionPool.new do
+            redis_instance
+          end
+        end
+      end
+
+      def self.redis_pool
+        @@redis
       end
 
       def self.store_fragment_in_tag(fragment, tag)
-        redis.sadd(tag, fragment)
+        redis_pool.with do |pool|
+          pool.sadd(tag, fragment)
+        end
       end
 
       def self.store_tags(tags)
-        tags.each { |tag| redis.sadd(Cashier::CACHE_KEY, tag) }
+        redis_pool.with do |pool|
+          tags.each { |tag| pool.sadd(Cashier::CACHE_KEY, tag) }
+        end
       end
 
       def self.remove_tags(tags)
-        tags.each { |tag| redis.srem(Cashier::CACHE_KEY, tag) }
+        redis_pool.with do |pool|
+          tags.each { |tag| pool.srem(Cashier::CACHE_KEY, tag) }
+        end
       end
 
       def self.tags
-        redis.smembers(Cashier::CACHE_KEY) || []
+        redis_pool.with do |pool|
+          pool.smembers(Cashier::CACHE_KEY) || []
+        end
       end
 
       def self.get_fragments_for_tag(tag)
-        redis.smembers(tag) || []
+        redis_pool.with do |pool|
+          pool.smembers(tag) || []
+        end
       end
 
       def self.delete_tag(tag)
-        redis.del(tag)
+        redis_pool.with do |pool|
+          pool.del(tag)
+        end
       end
 
       def self.clear
         remove_tags(tags)
-        redis.del(Cashier::CACHE_KEY)
+        redis_pool.with do |pool|
+          redis.del(Cashier::CACHE_KEY)
+        end
       end
 
       def self.keys
